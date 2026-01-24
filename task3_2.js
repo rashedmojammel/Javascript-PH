4ba28ad28fd14b53d4b9e854c575b10228c535d5
rashedmojammel <rashedmojamme56@gmail.com>
2026-01-25 00:29:17 +0600

    task3-2

---
+++ task3_2.js
@@ -0,0 +1,17 @@
+//  Add or remove elements
+// Declare an array of 3 tourist destinations
+// Add a new tourist destination to your tourist array
+// Add two more to your array
+// Remove the last tourist destination you have added
+// display the final array as output
+
+
+const tourist = ['rangamati' , 'bandarban', 'kaptai','coxsbajar', 'sylthet'];
+
+tourist.push('shrimongol');
+
+console.log(tourist);
+
+tourist.push('shrimongol','teknaf');
+
+console.log(tourist);
