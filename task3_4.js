2a62b45a4d3bca74427b3428370726b5c8603abf
rashedmojammel <rashedmojamme56@gmail.com>
2026-01-25 00:40:23 +0600

    addded 3_4

---
+++ task3_4.js
@@ -0,0 +1,19 @@
+// 4. Checking if it's an Array
+// Instructions:
+
+// Create different variables, each containing either an array or a non-array value.
+
+// Now use isArray to check if each variable is an array.
+
+// Print a message to the console indicating whether each variable is an array or not.
+
+
+const array=[];
+const num=2;
+
+
+let isArray= Array.isArray(array);
+let isArray2 = Array.isArray(num);
+
+console.log(isArray);
+console.log(isArray2);
