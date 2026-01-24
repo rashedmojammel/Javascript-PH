7ef07a30460c08a7b96cec66a44a252ebfedd34d
rashedmojammel <rashedmojamme56@gmail.com>
2026-01-25 00:24:54 +0600

    task3-1

---
+++ task3.js
@@ -0,0 +1,11 @@
+// 1. Declare an array
+// Declare an array with 5 elements containing fruits
+// console log the 3rd index element
+// change the value of the 2nd index element to jambura
+// console log the final array
+
+
+const name = ['rashed','alam','mojammel','ummul','fatema'];
+console.log(name[2]);
+name[1] = 'khayer';
+console.log(name[1]);
